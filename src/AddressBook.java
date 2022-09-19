9d85c91a0a9ec825b64064d189cfa74586404e0f
dankuch <therealdkuch@gmail.com>
2022-09-19 11:06:20 -0400

    Added method that prints "Address Book" Added test for removeBuddy and addBuddy methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -9,12 +9,16 @@ public class AddressBook {
 
     public static void main(String[] args){
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Dan","Carleton","613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
     public void addBuddy(BuddyInfo buddy) {
         buddies.add(buddy);
     }
 
-    public void removeBuddy() {
+    public void removeBuddy(BuddyInfo buddy) {
         buddies.remove();
     }
 }
